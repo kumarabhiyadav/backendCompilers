64a824c7d51c610359327d86dbec868cdda3308e
Abhinavkumar Yadav <mr.kumarabhinavyadav@gmail.com>
2022-02-18 17:04:30 +0530

    not working

--- compilers/compilers.js
+++ compilers/compilers.js
@@ -1,9 +1,17 @@
 import { execPromise } from "../utils/helper.js";
 
+const filesPath = "src/languageFiles";
+
 async function c_lang() {
   try {
-    let result = await execPromise("gcc main.c -o main");
-    result = await execPromise("main.exe");
+    let result = await execPromise("cd");
+    execPromise(`cd ${filesPath}/C`);
+    result = await execPromise("cd");
+
+    return result;
+
+    // let result = await execPromise("gcc main.c -o main");
+    // result = await execPromise("main.exe");
 
     console.log(result);
     return result;
@@ -15,6 +23,7 @@ async function c_lang() {
 
 async function python_lang() {
   try {
+    execPromise(`cd ${filesPath}/python`);
     let result = await execPromise("python main.py");
 
     console.log(result);
@@ -27,9 +36,9 @@ async function python_lang() {
 
 async function java_lang() {
   try {
+    execPromise(`cd ${filesPath}/java`);
     let result = await execPromise("javac HelloWorld.java");
-    result = await execPromise("cd languageFiles/java");
-
+    result = await execPromise(`cd ${filesPath}/java`);
     result = await execPromise("java HelloWorld");
 
     console.log(result);
